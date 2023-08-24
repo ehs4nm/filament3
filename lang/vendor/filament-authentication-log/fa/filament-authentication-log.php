d2fbde928a2276695d0dc786424d808b6ca9f535
ehs4nm <64464913+ehs4nm@users.noreply.github.com>
2023-08-24 10:52:18 +0330

    translation

---
+++ lang/vendor/filament-authentication-log/fa/filament-authentication-log.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'navigation.group' => 'ورودها',
+
+    'navigation.authentication-log.label' => 'سابقه ورود',
+    'navigation.authentication-log.plural-label' => 'سوابق ورود',
+
+    'table.heading' => 'سوابق ورود',
+
+    'column.authenticatable' => 'نوع',
+    'column.ip_address' => 'IP آدرس',
+    'column.user_agent' => 'کاربر Agent',
+    'column.login_at' => 'تاریخ ورود',
+    'column.login_successful' => 'ورود موفق',
+    'column.logout_at' => 'تاریخ خروج',
+    'column.cleared_by_user' => 'Cleared By User',
+];
