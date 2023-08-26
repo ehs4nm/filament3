66613e30746bffe553733b71281add59ff062211
ehs4nm <64464913+ehs4nm@users.noreply.github.com>
2023-08-26 16:16:11 +0330

    verify_code field cahnged to string

--- database/migrations/2023_08_25_073256_add_mobile_code_to_users_table.php
+++ database/migrations/2023_08_25_073256_add_mobile_code_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('verify_code')->nullable()->after('email');
+            $table->string('verify_code', 4)->nullable()->after('email');
         });
     }
 
